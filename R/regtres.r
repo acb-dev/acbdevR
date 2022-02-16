38b1a5eeb2db0b08aee51623bc4e44ed1eb82ede
acb-dev <acbdev@hotmail.com>
2022-02-16 14:55:00 -0300

    Criação da ajuda da função regtres

--- R/regtres.r
+++ R/regtres.r
@@ -1,29 +1,17 @@
-#' @title Faz a Soma de Dois Numeros
-#' @name soma
+#' @title Calcula regra de 3 simples.
+#' @name regtres
 #'
-#' @description Uma (incrivel) funcao que pega dois numeros e faz a
-#'     soma. Utilize este campo para descrever melhor o proposito de
-#'     sua funcao e o que ela e capaz de fazer.
+#' @description Calcula regra de 3 simples.
 #'
-#' @param x Um numero
-#' @param y Outro numero
+#' @param a, primeira grandeza
+#' @param b, segunda grandeza
+#' @param c, terceira grandeza
 #'
-#' @details Utilize este campo para escrever detalhes mais tecnicos da
-#'     sua funcao (se necessario), ou para detalhar melhor como
-#'     utilizar determinados argumentos.
-#'
-#' @return A soma dos numeros \code{x} e \code{y}.
-#'
-#' @author Fernando Mayer
-#'
-#' @seealso \code{\link[base]{sum}}, \code{\link[base]{+}}
+#' @author Alexandre Barbosa | acbdev@hotmail.com | +55 9.8370-8251
+
 #'
 #' @examples
-#' soma(2, 2)
-#'
-#' x <- 3
-#' y <- 4
-#' soma(x = x, y = y)
+#' regtres(10, 20, 100)
 #'
 #' @export
 regtres <- function(a,b,c) {
