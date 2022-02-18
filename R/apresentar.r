2fdfb84d2926ffd0e77c39fa36c1dbc1ee304386
acb-dev <acbdev@hotmail.com>
2022-02-18 17:41:01 -0300

    Finalização da função apresentar

--- R/apresentar.r
+++ R/apresentar.r
@@ -1,20 +1,29 @@
-#' @title Calcula valores da tabela SAC.
+#' @title Lista de funcoes do pacote.
 #'
 #' @name apresentar
 #'
 #' @description 
-#'Lista de funcoes contidas no pacote
 #'desconto(); Calculo de descontos.
+#' @description 
 #'juros(); Calcula juros tanto simples como compostos, conforme escolha do usuario.
+#' @description 
 #'jurocomposto(); Calcula juros compostos.
+#' @description 
 #'jurosimples(); Calcula juros simples.
-#'melhorpreco(); Determina qual produto entre 2 é o mais economico.
+#' @description 
+#'melhorpreco(); Determina qual produto entre 2 e o mais economico.
+#' @description 
 #'regtres(); Calcula regra de 3 simples.
+#' @description 
 #'resto(); Apresenta o resto da divisao exata, mas apresenta tambem o quociente da divisao com parte decimal.
+#' @description 
 #'tabprice(); Calcula o valor da prestacao pelo metodo de tabela Price.
-#'tabsac(); Calcula o valor das prestações pelo metodo de tabela SAC apresentando uma tabela na tela.
+#' @description 
+#'tabsac(); Calcula o valor das prestacoes pelo metodo de tabela SAC apresentando uma tabela na tela.
+#' @description 
 #'tabuada(); Exibe a tabuada do numero solicitado.
-#'Todas funões possuem ajuda disponível digitando o ponto de interrogacao antes de seus nomes sem os parenteses.
+#' @description 
+#'Todas funcoes possuem ajuda disponivel digitando o ponto de interrogacao antes de seus nomes sem os parenteses.
 #'
 #' @author Alexandre Barbosa | acbdev@hotmail.com | +55 9.8370-8251
 #' @export
@@ -24,7 +33,7 @@ print("desconto(); Calculo de descontos.")
 print("juros(); Calcula juros tanto simples como compostos, conforme escolha do usuário.")
 print("jurocomposto(); Calcula juros compostos.")
 print("jurosimples(); Calcula juros simples.")
-print("melhorpreco(); Determina qual produto entre 2 é o mais economico.")
+print("melhorpreco(); Determina qual produto entre 2 e o mais economico.")
 print("regtres(); Calcula regra de 3 simples.")
 print("resto(); Apresenta o resto da divisao exata, mas apresenta tambem o quociente da divisao com parte decimal.")
 print("tabprice(); Calcula o valor da prestacao pelo metodo de tabela Price.")
