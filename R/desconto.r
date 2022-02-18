f29fc9bfcc1d10f1c5bc51c820fa1db5cdc21781
acb-dev <acbdev@hotmail.com>
2022-02-18 10:17:12 -0300

    Alterações diversas

--- R/desconto.r
+++ R/desconto.r
@@ -1,3 +1,13 @@
+#' @title Calculo de Desconto.
+#'
+#' @name desconto
+#'
+#' @description Siga as instrucoes da tela e forneca as informacoes solicitadas para obter o resultado.
+#'
+#' @author Alexandre Barbosa | acbdev@hotmail.com | +55 9.8370-8251
+#' @examples
+#' desconto()
+#' @export
 desconto <- function () {
 print("Preço: ")
 preco <- scan(nmax=1)
@@ -7,9 +17,9 @@ perc <- scan(nmax=1)
 desc <- preco * (perc/100)
 liquido <- preco - desc
 
-linha1 <- paste("Valor total      : ", formatC(preco, decimal.mark = ",", big.mark = ".", width = 15, format = "f"))
-linha2 <- paste("Valor do Desconto: ", formatC(desc, decimal.mark = ",", big.mark = ".", width = 15, format = "f"))
-linha3 <- paste("Valor a se pago  : ", formatC(liquido, decimal.mark = ",", big.mark = ".", width = 15, format = "f"))
+linha1 <- paste("Valor original    : ", formatC(preco, decimal.mark = ",", big.mark = ".", width = 15, format = "f", digits = 2))
+linha2 <- paste("Valor do Desconto : ", formatC(desc, decimal.mark = ",", big.mark = ".", width = 15, format = "f", digits = 2))
+linha3 <- paste("Valor a ser pago  : ", formatC(liquido, decimal.mark = ",", big.mark = ".", width = 15, format = "f", digits = 2))
 
 print(linha1)
 print(linha2)
--- R/jurocomposto.r
+++ R/jurocomposto.r
@@ -3,7 +3,7 @@
 #'
 #'
 #' @param c, Capital investido.
-#' @param i, A taxa de juros representada por um numero inteiro.
+#' @param i, A taxa de juros.
 #' @param t, quantidade de periodos, ex: quantidade de meses.
 #'
 #' @author Alexandre Barbosa | acbdev@hotmail.com | +55 9.8370-8251
@@ -16,5 +16,11 @@ i <- i/100
 
 j <- (c*(1 + i)^t) - c
 
-print(j)
+linha1 <- paste("Capital : ", formatC(c, decimal.mark = ",", big.mark = ".", width = 15, format = "f", digits = 2))
+linha2 <- paste("Juros   : ", formatC(j, decimal.mark = ",", big.mark = ".", width = 15, format = "f", digits = 2))
+linha3 <- paste("Montante: ", formatC(c + j, decimal.mark = ",", big.mark = ".", width = 15, format = "f", digits = 2))
+
+print(linha1)
+print(linha2)
+print(linha3)
 }
