a3bd419d7cccdc5bc131495b30ec6826d9a0d01f
acb-dev <acbdev@hotmail.com>
2022-02-19 16:29:32 -0300

    Versão 1.01

--- R/tabsac.r
+++ R/tabsac.r
@@ -33,6 +33,12 @@ j <- 0
 # Inicialização da prestação
 p <- 0
 
+# Inicialização do valor total das prestações
+pp <- 0
+
+# Inicialização do valor total dos juros
+jj <- 0
+
 parcela <- 0
 
 print("┌--------------------------------------------------------------------------------┐")
@@ -42,6 +48,10 @@ for (p in 1:n) {
 parcela <- parcela + 1
 j <- i * sd
 p <- q + j
+
+jj <- jj + j
+pp <- pp + p
+
 #result <- paste("|Parcela|", format(parcela, nsmall = 0, width = 3) , "|Saldo|", format(sd, nsmall = 2, width = 9), #"|Amotização|", format(a, nsmall = 2, width = 9), "|Quota de Amortização|", format(q, nsmall = 2, width = 9) , #"|Juros|", format(j, nsmall = 2, width = 9), "|Prestação|", format(p, nsmall =2, width = 9),"|")
 result <- paste("|", format(parcela, nsmall = 0, width = 5) , "|", format(sd, nsmall = 2, width = 9, decimal.mark = ",", digits = 2), "|", format(a, nsmall = 2, width = 11, decimal.mark = ",", digits = 2), "|", format(q, nsmall = 2, width = 20, decimal.mark = ",", digits = 2) , "|", format(j, nsmall = 2, width = 9, decimal.mark = ",", digits = 2), "|", format(p, nsmall =2, width = 9, decimal.mark = ",", digits = 2),"|")
 print(result)
@@ -49,4 +59,9 @@ print("└----------------------------------------------------------------------
 sd <- sd - q
 a <- a + q
 }
+linha1 <- paste("Total geral dos juros        ==>", format(jj, nsmall = 2, width = 9, big.mark = ".", decimal.mark = ",", digits = 2))
+linha2 <- paste("Total geral do financiamento ==>", format(pp, nsmall = 2, width = 9, big.mark = ".", decimal.mark = ",", digits = 2))
+
+print(linha1)
+print(linha2)
 }
